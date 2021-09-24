7f74b1bf8c2a3aaecd71fe94f0bafcd9b957c622
Asal Shamim <41086746+asalshamim@users.noreply.github.com>
2021-09-24 15:31:15 +0200

    1

---
+++ Labs/Lab2/lab.py
@@ -0,0 +1,58 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "source": [
+    "# pichu & Pikachu"
+   ],
+   "metadata": {}
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "source": [
+    "import matplotlib.pyplot as plt\r\n",
+    "\r\n",
+    "p_pikachu = \"pichu.text\"\r\n",
+    "p_pichu= \"pikachu.text\"\r\n",
+    " with open (pa_pikachu, \"r\") as data_pikachu, open (p_pichu, \"r\") as data_pichu:\r\n",
+    "pichu= [pichu.strip(\"\\n\" \"()\").split(\",\") for pichu in data_pichu.readline()]     "
+   ],
+   "outputs": [
+    {
+     "output_type": "error",
+     "ename": "IndentationError",
+     "evalue": "unexpected indent (Temp/ipykernel_3680/3646926831.py, line 5)",
+     "traceback": [
+      "\u001b[1;36m  File \u001b[1;32m\"C:\\Users\\asals\\AppData\\Local\\Temp/ipykernel_3680/3646926831.py\"\u001b[1;36m, line \u001b[1;32m5\u001b[0m\n\u001b[1;33m    with open (pa_pikachu, \"r\") as data_pikachu, open (p_pichu, \"r\") as data_pichu:\u001b[0m\n\u001b[1;37m    ^\u001b[0m\n\u001b[1;31mIndentationError\u001b[0m\u001b[1;31m:\u001b[0m unexpected indent\n"
+     ]
+    }
+   ],
+   "metadata": {}
+  }
+ ],
+ "metadata": {
+  "orig_nbformat": 4,
+  "language_info": {
+   "name": "python",
+   "version": "3.9.6",
+   "mimetype": "text/x-python",
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "pygments_lexer": "ipython3",
+   "nbconvert_exporter": "python",
+   "file_extension": ".py"
+  },
+  "kernelspec": {
+   "name": "python3",
+   "display_name": "Python 3.9.6 64-bit"
+  },
+  "interpreter": {
+   "hash": "5a5fe90e65c548f81cb78b086af2a72a1ccb18c5803a011cbeaa15cf0cb151fe"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
